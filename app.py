5ff2b49e21d5f5227f5b79290b62b839b5d91d35
yahav123456 <166650066+yahav123456@users.noreply.github.com>
2024-05-31 02:45:25 +0300

    yy

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world Yahav gay  '
+    return 'Hello world i'm Yahav! :) '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
